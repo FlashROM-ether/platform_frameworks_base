827b14196213be6c17266f80390fce943b45e475
bigrushdog <randall.rushing@gmail.com>
2019-10-22 23:51:43 +0100

    Screen off animations [1/2]
    Fade (default), CRT, and Scale
    
    Credit and respect to @xplodwild for paving the way
    back in the KitKat days!
    
    History of the ElectronBeam class can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/kitkat/services/java/com/android/server/power/ElectronBeam.java
    
    Adapted to 10 by @eyosen
    
    Change-Id: I58d269d44c901a8c0471807e3cf05c2054205d28

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5446,6 +5446,12 @@ public final class Settings {
          public static final String STATUS_BAR_LOGO_COLOR = "status_bar_logo_color";
 
         /**
+         * Defines the screen-off animation to display
+         * @hide
+         */
+        public static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
